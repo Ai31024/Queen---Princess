d778a4097b1ca36fb9261860e381105bc6f7de53
Mohammad Azam Irshad <iazam44@hotmail.com>
2024-08-22 17:14:37 +0530

    Enhanced to standard

--- script.js
+++ script.js
@@ -1,46 +1,82 @@
-let girl1 = document.querySelectorAll("img")[0];
-let girl2 = document.querySelectorAll("img")[1];
-let girl3 = document.querySelectorAll("img")[2];
+// Function to set up event listeners for changing and reverting image and heading
+function setupButtonActions(
+  buttonChange,
+  buttonRevert,
+  imgElement,
+  newImgSrc,
+  originalImgSrc,
+  headingElement,
+  newHeading,
+  originalHeading
+) {
+  if (buttonChange && buttonRevert && imgElement && headingElement) {
+    // Event Listener for Change button
+    buttonChange.addEventListener("click", function () {
+      imgElement.src = newImgSrc;
+      headingElement.textContent = newHeading;
+    });
 
-let subHeading1 = document.querySelectorAll("h3")[0];
-let subHeading2 = document.querySelectorAll("h3")[1];
-let subHeading3 = document.querySelectorAll("h3")[2];
+    // Event Listener for Revert button
+    buttonRevert.addEventListener("click", function () {
+      imgElement.src = originalImgSrc;
+      headingElement.textContent = originalHeading;
+    });
+  } else {
+    console.error("One or more elements are missing.");
+  }
+}
 
-let button1A = document.querySelector("#button1A");
-let button1B = document.querySelector("#button1B");
+// Querying articles
+const article1 = document.querySelectorAll("article")[0];
+const article2 = document.querySelectorAll("article")[1];
+const article3 = document.querySelectorAll("article")[2];
 
-let button2A = document.querySelector("#button2A");
-let button2B = document.querySelector("#button2B");
+// Querying elements for the first section
+const girl1 = article1.querySelector("img");
+const subHeading1 = article1.querySelector("h3");
+const button1A = document.querySelector("#button1A");
+const button1B = document.querySelector("#button1B");
 
-let button3A = document.querySelector("#button3A");
-let button3B = document.querySelector("#button3B");
+// Querying elements for the second section
+const girl2 = article2.querySelector("img");
+const subHeading2 = article2.querySelector("h3");
+const button2A = document.querySelector("#button2A");
+const button2B = document.querySelector("#button2B");
 
-button1A.addEventListener("click", function () {
-  girl1.src = "./media/girl_1A.jpeg";
-  subHeading1.textContent = "Queen";
-});
+// Querying elements for the third section
+const girl3 = article3.querySelector("img");
+const subHeading3 = article3.querySelector("h3");
+const button3A = document.querySelector("#button3A");
+const button3B = document.querySelector("#button3B");
 
-button1B.addEventListener("click", function () {
-  girl1.src = "./media/girl_1.jpeg";
-  subHeading1.textContent = "Princess";
-});
-
-button2A.addEventListener("click", function () {
-  girl2.src = "./media/girl_2A.jpeg";
-  subHeading2.textContent = "Queen";
-});
-
-button2B.addEventListener("click", function () {
-  girl2.src = "./media/girl_2.jpeg";
-  subHeading2.textContent = "Princess";
-});
-
-button3A.addEventListener("click", function () {
-  girl3.src = "./media/girl_3A.jpeg";
-  subHeading3.textContent = "Queen";
-});
-
-button3B.addEventListener("click", function () {
-  girl3.src = "./media/girl_3.jpeg";
-  subHeading3.textContent = "Princess";
-});
+// Set up button actions for each section
+setupButtonActions(
+  button1A,
+  button1B,
+  girl1,
+  "./media/girl_1A.jpeg",
+  "./media/girl_1.jpeg",
+  subHeading1,
+  "Queen",
+  "Princess"
+);
+setupButtonActions(
+  button2A,
+  button2B,
+  girl2,
+  "./media/girl_2A.jpeg",
+  "./media/girl_2.jpeg",
+  subHeading2,
+  "Queen",
+  "Princess"
+);
+setupButtonActions(
+  button3A,
+  button3B,
+  girl3,
+  "./media/girl_3A.jpeg",
+  "./media/girl_3.jpeg",
+  subHeading3,
+  "Queen",
+  "Princess"
+);
